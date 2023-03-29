5d675c557537a9d76c421816f2c57343060f2dfa
JuliaC00 <sweettoothfoodtography@gmail.com>
2023-03-29 16:18:17 -0700

    the basic structure of the car

---
+++ src/BasicStructure.java
@@ -0,0 +1,3 @@
+public class BasicStructure {
+    String color;
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+       BasicStructure audi=new BasicStructure();
+       audi.color()="black";
+    }
+}
